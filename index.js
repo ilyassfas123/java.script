49bfd2f212e732ca546f73b5cf31cb643a70505e
ilyassfas123 <ilyassfaskaa123@gmail.com>
2023-02-22 12:20:18 +0100

    hello

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+const str ="Créez votre premier site web"
+console.log("nombre de caractére " ,str.length)
+*/
+
+/*
+const str = "Créez votre premier site web";
+const mots = str.split(" ");
+const nombreDeMots = mots.length;
+console.log(nombreDeMots);
+*/
+
+/*
+var phrase = "Créez votre premier site web";
+var nombreDeVoyelles = 0;
+const voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+for (let i = 0; i < phrase.length; i++) {
+    if (voyelles.includes(phrase[i])) {
+        nombreDeVoyelles++;
+    }
+}
+
+console.log("Le nombre de voyelles dans la phrase est: " + nombreDeVoyelles);
+*/
+
+
+

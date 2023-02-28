7dba732f9357af3c06266cb8ed4418415f0ad95c
ilyassfas123 <ilyassfaskaa123@gmail.com>
2023-02-28 16:00:11 +0100

    hello

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const nombreDeMots = mots.length;
 console.log(nombreDeMots);
 */
 
-/*
+
 var phrase = "Créez votre premier site web";
 var nombreDeVoyelles = 0;
 const voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
@@ -22,7 +22,7 @@ for (let i = 0; i < phrase.length; i++) {
 }
 
 console.log("Le nombre de voyelles dans la phrase est: " + nombreDeVoyelles);
-*/
+
 
 
 
